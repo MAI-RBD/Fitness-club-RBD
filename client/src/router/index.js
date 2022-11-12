aafe980429c383819aec05e74261c711c6dd4af4
MimkaKek <grislava@gmail.com>
2022-11-12 21:29:10 +0300

    Update

---
+++ client/src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import PingView from '../views/PingView.vue'
+import TestDBView from '../views/TestDBView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/ping',
+      name: 'ping',
+      component: PingView
+    },
+    {
+      path: '/test_db',
+      name: 'testDB',
+      component: TestDBView
+    }
+  ]
+})
+
+export default router
